e3671babec98fcfc8e3c8f9707913dc5eeb4ee84
NamelessOgya <hakuginnnohana@gmail.com>
2025-06-20 10:51:15 +0000

    Auto: Run experiment with id: exp_07d2a57e7bb2

---
+++ run_experiment.sh
@@ -0,0 +1,17 @@
+# ① タイムスタンプ取得
+timestamp=$(date +%Y%m%d%H%M%S)
+
+# ② ハッシュ生成（SHA-1 or SHA-256 どちらでも可）
+hash=$(echo -n "$timestamp" | sha1sum | cut -c1-12)
+run_id="exp_${hash}"
+
+echo "=========================="
+echo "experinent id: $run_id"
+echo "=========================="
+
+
+dvc repro --params run.id="$run_id"
+
+git add .
+git commit -m "Auto: Run experiment with id: $run_id"
+git tag $run_id
