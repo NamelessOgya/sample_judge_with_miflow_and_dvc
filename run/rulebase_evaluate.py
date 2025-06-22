e87f86cfc4878d9b9fd191f09acdc365ad87a663
NamelessOgya <hakuginnnohana@gmail.com>
2025-06-22 01:22:13 +0000

    Auto: Run experiment with id: exp_4efc5d43e4f4

--- run/rulebase_evaluate.py
+++ run/rulebase_evaluate.py
@@ -30,6 +30,8 @@ def main():
     
     result.to_json(f"./data/result/rulebase_{args.submit_file_name}_{args.rulebase_func_name}.json", orient="records", lines=True, force_ascii=False)
 
+    mlflow.set_experiment("evaluate")
+
     mlflow.log_param("mikoto_run_id", get_current_run_id())
     mlflow.log_param("category", "rulebase")
     mlflow.log_param("rulebase_func_name", judge.rulebase_func_name)
