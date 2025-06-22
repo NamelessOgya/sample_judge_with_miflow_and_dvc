9df9e86c4f96e9b03e3a91649ffbd4f3a985dffc
NamelessOgya <hakuginnnohana@gmail.com>
2025-06-22 03:59:54 +0000

    Auto: Run experiment with id: exp_e3d696648c87

--- run/llm_evaluate.py
+++ run/llm_evaluate.py
@@ -43,7 +43,7 @@ def main():
     
     if "filter" in judge_menu.keys():
         for condition in judge_menu["filter"]:
-            print("#########)
+            print("#########")
             print(condition)
             submit = submit[submit[condition["filter_col_name"]].astype(str) == condition["filter_value"].astype(str)].copy()
     else:
